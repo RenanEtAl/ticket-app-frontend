af743f8a71aeda831e8ce34097904989e49710d4
Renanie Tanola <enan.tanola@gmail.com>
2020-07-23 23:25:11 -0700

    redux setup

--- ticket-fe/src/App.js
+++ ticket-fe/src/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+//redux
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 function App() {
   return (
-    <Router>
-      <div className="App container">
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/sign-in" component={Login} />
-          <Route exact path="/sign-up" component={Register} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App container">
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/sign-in" component={Login} />
+            <Route exact path="/sign-up" component={Register} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ ticket-fe/src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+
+const appReducers = combineReducers({
+    auth: '',
+    errors: '',
+
+})
+
+
+export default rootReducers
