eb16d7854e143861beca8de3c7886f6ceb31a620
Renanie Tanola <enan.tanola@gmail.com>
2020-07-24 21:34:25 -0700

    fixed card component

--- ticket-fe/src/components/main/card/Card.jsx
+++ ticket-fe/src/components/main/card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import _ from "lodash";
 import { connect } from "react-redux";
-import "./Card.css";
+import "./Card.css"
 import { Box } from "./Box";
 import { Button } from "../../reusable/Button";
 
